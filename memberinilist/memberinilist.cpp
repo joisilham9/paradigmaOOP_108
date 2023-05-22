468c7d536accdcdcf4bd1cb90483b89a12b36a4a
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-22 18:31:21 +0700

    kelas mahasiswa

---
+++ memberinilist/memberinilist.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+
+	}
+	~mahasiswa() {
+		cout << "id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
