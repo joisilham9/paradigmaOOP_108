c26c99a56e667a3c1072bab7764133d50af17cf3
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-22 15:54:57 +0700

    perintah di dalam kelas dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -8,12 +8,27 @@ public:
 	string nama;
 	vector<dokter*> daftar_dokter;
 	pasien(string pNama) :nama(pNama) {
-		cout << "pasien \"" << nama << "\" ada\n";
+		cout << " pasien \"" << nama << "\" ada\n";
 	}
 	~pasien() {
-		cout << "pasien \"" << nama << "\" tidak ada\n";
+		cout << " pasien \"" << nama << "\" tidak ada\n";
 	}
 	void tamabahDokter(dokter*);
 	void cetakDokter();
 };
 
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << " Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << " Dokter \"" << nama << "\" ada\n";
+	}
+
+	void tambahkanPasien(pasien*);
+	void cetakPasien();
+};
